eb7958bca0a9438aaabf0b3f5269b1b85b1d0121
Nikhil <harshita10203040@gmail.com>
2024-04-23 11:07:14 +0530

    phone numbers

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -38,6 +38,7 @@ export function initAdmin(socket) {
                 </td>
                 <td class="border px-4 py-2">${ order.customerId.name }</td>
                 <td class="border px-4 py-2">${ order.address }</td>
+                <td class="border px-4 py-2">${ order.phone }</td>
                 <td class="border px-4 py-2">
                     <div class="inline-block relative w-64">
                         <form action="/admin/orders/status" method="post">
